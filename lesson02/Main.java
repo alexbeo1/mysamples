635e0c04ea71c137cde38c2ee6f1a18b16d3f589
alexbeo1 <wec@tut.by>
2015-08-27 18:51:49 +0300

    Implements equals

--- lesson02/Main.java
+++ lesson02/Main.java
@@ -1,11 +1,20 @@
 package lesson02;
 
+import ru.oserbii.webapp.model.Link;
+
 /**
  * Александр Козлов
  * 27.08.2015.
  */
 public class Main {
     public static void main(String[] args) {
+
+        Link l1 = new Link("Alex Kozloff", "www.oserbii.ru");
+        Link l2 = l1;
+        Link l3 = new Link(l1);
+        System.out.println(l1.getClass());
+        System.out.println(l1.equals(l2));
+        System.out.println(l1.equals(l3));
 
     }
 }
